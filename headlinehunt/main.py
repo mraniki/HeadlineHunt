930e4b7ee551bd18332a9e453321be05a113d74d
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-19 12:30:37 +0200

    ♻️ Provider structure

--- headlinehunt/main.py
+++ headlinehunt/main.py
@@ -1,21 +1,20 @@
 import urllib
 
+import aiohttp
 import feedparser
 import requests
+import xmltodict
 from bs4 import BeautifulSoup
 from dateparser import parse as parse_date
 from loguru import logger
-import aiohttp
-import xmltodict
+
 from headlinehunt.config import settings
+from headlinehunt.provider import AlphaVantage, FinnHub, GNews, GoogleNews, NewsPlease
 
-#class Newsroom
-
-
-
-class Headliner():
+# class Newsroom
 
 
+class Headliner:
     def __init__(self):
         """
         Initialize the Headliner class
@@ -31,9 +30,11 @@ class Headliner():
         self.news_feed = settings.news_rss_url
         self.news_source = None
         self.search_source = None
-        
 
     async def get_headliner_info(self):
+        return
+
+    async def get_provider(self):
         return
 
     async def fetch_feed(self):
@@ -54,20 +55,3 @@ class Headliner():
                 title = data["title"]
                 link = data["link"]
                 return f"📰 <a href='{link}'>{title}</a>"
-
-# async def fetch_top_news(self):
-#     try:
-#         async with aiohttp.ClientSession() as session:
-#             async with session.get(self.news_url, timeout=10) as response:
-#                 data = await response.json()
-#                 articles = data.get('articles', [])
-#                 key_news = [
-#                     {'title': article['title'], 'url': article['url']}
-#                     for article in articles
-#                 ]
-#                 last_item = key_news[-1]
-#                 return f"📰 <a href='{last_item['url']}'>{last_item['title']}</a>"
-
-#     except aiohttp.ClientError as error:
-#         self.logger.warning("news %s", error)
-#         return None
